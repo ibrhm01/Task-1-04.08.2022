98b9549fcfc75edf2b48b6c9ad9ce5f5e2b521f4
Ibrahim Aliyev <ibrahimma@code.edu.az>
2022-08-04 18:09:07 +0400

    Task(1)

---
+++ Task(1)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Min(45,91,63,10,120,3,15));
+        }
+        static int Min(params int[] arr)
+        {
+            int min = arr[0];
+            for(int i = 1; i < arr.Length; i++)
+            {
+                if (min > arr[i]) min = arr[i];
+            }
+            return min;
+        }
+
+    }
+}
